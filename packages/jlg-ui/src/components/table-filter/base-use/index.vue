35fbbc9c4eb123fac70aa7d4895b9ddbd81e16d9
DESKTOP-SGL7UB9\17680 <17680326373@163.com>
2024-04-27 17:43:08 +0800

    feat: table

--- packages/jlg-ui/src/components/table-filter/base-use/index.vue
+++ packages/jlg-ui/src/components/table-filter/base-use/index.vue
@@ -1,13 +1,30 @@
 <template>
-	<!--	<pre>{{ items }}</pre>-->
-	<TableFilter v-model:items="items"></TableFilter>
-	<el-button @click="onClick">修改searchType</el-button>
+	<table-grid ref="jlgGrid" v-bind="gridOptions" style="height: 95vh">
+		<template #toolbar_buttons>
+			<vxe-button @click="handleFlushed">刷新</vxe-button>
+			<vxe-button @click="handleAdd">新增</vxe-button>
+			<vxe-button @click="handleDelete">删除</vxe-button>
+			<vxe-button @click="handleDelete2">删除2</vxe-button>
+			<vxe-button>保存</vxe-button>
+			<vxe-button>导出</vxe-button>
+		</template>
+		<template #default_sleepDuration>
+			<img width="50" src="https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg" alt=""
+		/></template>
+		<template #default_button>
+			<el-button>default_sleepDuration</el-button>
+		</template>
+	</table-grid>
+	<!--	<TableFilter v-model:items="items"></TableFilter>-->
 </template>
 
 <script setup lang="ts">
-import TableFilter from '@pac/table-filter/index.vue';
+import TableGrid from '../../../../packages/table-base/index.vue';
+// import TableFilter from '@pac/table-filter/index.vue';
 import { I_Table_Filter_Item } from '../../../../packages/table-filter/type';
 import { onMounted, reactive, ref } from 'vue';
+import { I_Table_Grid_Props, JlgGridInstance } from '../../../../packages/table-base/type';
+import { ElMessage } from 'element-plus';
 
 defineOptions({
 	name: 'TableFilterBaseUse',
@@ -15,6 +32,7 @@ defineOptions({
 
 const options = ref<ListItem[]>([]);
 const loading = ref(false);
+const jlgGrid = ref<JlgGridInstance<RowVO>>();
 const remoteMethod = (query: string) => {
 	if (query !== '') {
 		loading.value = true;
@@ -287,8 +305,230 @@ onMounted(() => {
 	}, 2000);
 });
 
-function onClick() {
-	items[0].searchType = 0;
+interface RowVO {
+	id: number;
+	name: string;
+	nickname: string;
+	role: string;
+	sex: string;
+	age: number;
+	address: string;
+	// 生日
+	birthday: string;
+	// 身高
+	height: number;
+	// 体重
+	weight: number;
+	// 体脂率
+	bodyFatRate: number;
+	// 体型
+	bodyShape: string;
+	// 体质指数
+	bmi: number;
+	// 肺活量
+	lungCapacity: number;
+	// 血压
+	bloodPressure: string;
+	// 心率
+	heartRate: number;
+	// 血氧
+	bloodOxygen: number;
+	// 体温
+	bodyTemperature: number;
+	// 睡眠时长
+	sleepDuration: number;
+}
+
+const gridOptions = reactive<I_Table_Grid_Props<RowVO>>({
+	border: true,
+	height: 'auto',
+	align: null,
+	columnConfig: {
+		resizable: true,
+	},
+	columns: [
+		{ type: 'checkbox', width: 50 },
+		{ type: 'seq', width: 60 },
+		{ field: 'name', title: '名字', width: 100 },
+		{ field: 'nickname', title: '昵称', width: 100 },
+		{ field: 'role', title: '角色', width: 100 },
+		{ field: 'address', title: '地址', showOverflow: true, width: 100 },
+		{ field: 'sex', title: '性别', showOverflow: true, width: 100 },
+		{ field: 'age', title: '年龄', width: 100 },
+		{ field: 'birthday', title: '生日', width: 100 },
+		{ field: 'height', title: '身高', width: 100 },
+		{ field: 'weight', title: '体重', width: 100 },
+		{ field: 'bodyFatRate', title: '体脂率', width: 100 },
+		{ field: 'bodyShape', title: '体型', width: 100 },
+		{ field: 'bmi', title: '体质指数', width: 100 },
+		{ field: 'lungCapacity', title: '肺活量', slots: { default: 'default_button' }, width: 100 },
+		{ field: 'bloodPressure', title: '血压', slots: { default: 'default_button' }, width: 100 },
+		{ field: 'heartRate', title: '心率', slots: { default: 'default_button' }, width: 100 },
+		{ field: 'bloodOxygen', title: '血氧', slots: { default: 'default_button' }, width: 100 },
+		{ field: 'bodyTemperature', title: '体温', slots: { default: 'default_button' }, width: 100 },
+		{ field: 'sleepDuration', title: '睡眠时长', slots: { default: 'default_sleepDuration' }, width: 100 },
+	],
+	proxyConfig: {
+		seq: true, // 启用动态序号代理（分页之后索引自动计算为当前页的起始序号）
+		message: true, // 启用消息提示代理
+		props: {
+			result: 'result',
+			total: 'page.total',
+		},
+		ajax: {
+			// 查询方法，可以通过 proxy-config.props 配置读取响应结构的字段；接收 Promise
+			query: ({ page, sort, sorts, filters, form }) => {
+				console.log('page', page);
+				console.log('sort', sort);
+				console.log('sorts', sorts);
+				console.log('filters', filters);
+				console.log('form', form);
+				return findPageList(page.currentPage, page.pageSize);
+			},
+			// 删除方法，提交的参数 { removeRecords }
+			delete: ({ body }) => {
+				console.log('body', body.removeRecords);
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						body.removeRecords.forEach((record) => {
+							console.log('record', record);
+							jlgGrid.value?.commitProxy('remove', false);
+							resolve({ message: 'success' });
+						});
+					}, 100);
+				});
+			},
+		},
+	},
+	toolbarConfig: {
+		custom: true,
+		slots: {
+			buttons: 'toolbar_buttons',
+		},
+	},
+	pagerConfig: {
+		enabled: true,
+		pageSize: 99,
+	},
+	tableFilterConfig: {
+		titleAlign: 'right',
+		folding: true,
+		items: items,
+	},
+});
+
+// 模拟分页接口
+const findPageList = (currentPage: number, pageSize: number) => {
+	return new Promise<{
+		page: {
+			total: number;
+		};
+		message: string;
+		result: RowVO[];
+	}>((resolve) => {
+		setTimeout(() => {
+			const list = generateData();
+			resolve({
+				page: {
+					total: list.length,
+				},
+				message: 'success',
+				result: list.slice((currentPage - 1) * pageSize, currentPage * pageSize),
+			});
+		}, 100);
+	});
+};
+
+const generateRandomRow = () => {
+	const randomSex = Math.random() < 0.5 ? 'Male' : 'Female';
+	const randomAge = Math.floor(Math.random() * 60) + 18; // Random age between 18 and 77
+	const randomHeight = Math.floor(Math.random() * 40) + 150; // Random height between 150 and 190 cm
+	const randomWeight = Math.floor(Math.random() * 60) + 40; // Random weight between 40 and 100 kg
+	const randomBodyFatRate = Math.random() * 30; // Random body fat rate between 0 and 30%
+	const randomBodyShape = Math.random() < 0.5 ? 'Slim' : 'Athletic';
+	const randomBMI = Math.random() * 10 + 18; // Random BMI between 18 and 28
+	const randomLungCapacity = Math.floor(Math.random() * 2000) + 3000; // Random lung capacity between 3000 and 5000 ml
+	const randomBloodPressure = `${Math.floor(Math.random() * 50) + 90}/${Math.floor(Math.random() * 50) + 60}`; // Random blood pressure between 90/60 and 140/110 mmHg
+	const randomHeartRate = Math.floor(Math.random() * 50) + 60; // Random heart rate between 60 and 110 bpm
+	const randomBloodOxygen = Math.random() * 20 + 80; // Random blood oxygen level between 80 and 100%
+	const randomBodyTemperature = Math.random() * 2 + 36.5; // Random body temperature between 36.5 and 38.5°C
+	const randomSleepDuration = Math.floor(Math.random() * 7) + 5; // Random sleep duration between 5 and 12 hours
+
+	return {
+		id: Math.floor(Math.random() * 100000), // Random ID
+		name: 'User' + Math.floor(Math.random() * 100), // Random name
+		nickname: 'Nickname' + Math.floor(Math.random() * 100), // Random nickname
+		role: 'Role' + Math.floor(Math.random() * 5), // Random role
+		sex: randomSex,
+		age: randomAge,
+		address: 'Address' + Math.floor(Math.random() * 100), // Random address
+		birthday: 'YYYY-MM-DD', // Placeholder birthday
+		height: randomHeight,
+		weight: randomWeight,
+		bodyFatRate: randomBodyFatRate,
+		bodyShape: randomBodyShape,
+		bmi: randomBMI,
+		lungCapacity: randomLungCapacity,
+		bloodPressure: randomBloodPressure,
+		heartRate: randomHeartRate,
+		bloodOxygen: randomBloodOxygen,
+		bodyTemperature: randomBodyTemperature,
+		sleepDuration: randomSleepDuration,
+	};
+};
+
+const generateData = () => {
+	const data = [];
+	for (let i = 0; i < 999; i++) {
+		data.push(generateRandomRow());
+	}
+	return data;
+};
+
+function handleFlushed() {
+	jlgGrid.value?.commitProxy('reload', true);
+}
+// 新增
+function handleAdd() {
+	jlgGrid.value?.commitProxy('insert', false, {
+		id: Math.floor(Math.random() * 100000),
+		name: 'User' + Math.floor(Math.random() * 100),
+		nickname: 'Nickname' + Math.floor(Math.random() * 100),
+		role: 'Role' + Math.floor(Math.random() * 5),
+		sex: 'Male',
+		age: Math.floor(Math.random() * 60) + 18,
+		address: 'Address' + Math.floor(Math.random() * 100),
+		birthday: 'YYYY-MM-DD',
+		height: Math.floor(Math.random() * 40) + 150,
+		weight: Math.floor(Math.random() * 60) + 40,
+		bodyFatRate: Math.random() * 30,
+		bodyShape: Math.random() < 0.5 ? 'Slim' : 'Athletic',
+		bmi: Math.random() * 10 + 18,
+		lungCapacity: Math.floor(Math.random() * 2000) + 3000,
+		bloodPressure: `${Math.floor(Math.random() * 50) + 90}/${Math.floor(Math.random() * 50) + 60}`,
+		heartRate: Math.floor(Math.random() * 50) + 60,
+		bloodOxygen: Math.random() * 20 + 80,
+		bodyTemperature: Math.random() * 2 + 36.5,
+		sleepDuration: Math.floor(Math.random() * 7) + 5,
+	});
+}
+// 前端删除
+function handleDelete() {
+	jlgGrid.value?.commitProxy('remove', () => {
+		const selectRecords = jlgGrid.value?.xGrid.getCheckboxRecords();
+		if (selectRecords.length > 3) {
+			ElMessage({
+				message: '一次最多只能删除3条数据!',
+				type: 'warning',
+			});
+			return false;
+		}
+		return true;
+	});
+}
+// 后端删除
+function handleDelete2() {
+	jlgGrid.value?.commitProxy('delete', true);
 }
 </script>
 
--- packages/jlg-ui/src/main.ts
+++ packages/jlg-ui/src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import '@pac/theme.scss';
 import '../styles/cssvar.scss';
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
+import 'element-plus/dist/index.css';
+
+function useTable(app) {
+	app.use(VXETable);
+}
 
 const app = createApp(App);
-app.mount('#app');
+app.use(useTable).mount('#app');
