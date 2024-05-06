8aeb1758f518764261114b14322b05ed2ef5d0b4
DESKTOP-SGL7UB9\17680 <17680326373@163.com>
2024-05-06 09:08:59 +0800

    build: 合并分支

--- packages/jlg-ui/src/App.vue
+++ packages/jlg-ui/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-	<table-filter-base-use> </table-filter-base-use>
+	<grid-layout-base-use> </grid-layout-base-use>
 </template>
 
 <script setup lang="ts">
 // import MenuBaseUse from '@/components/menu/base-use/index.vue';
-// import GridLayoutBaseUse from '@/components/grid-layout/base-use/index.vue';
-import TableFilterBaseUse from '@/components/table-filter/base-use/index.vue';
+import GridLayoutBaseUse from '@/components/grid-layout/base-use/index.vue';
 </script>
 
 <style></style>
--- packages/jlg-ui/src/components/table-filter/base-use/index.vue.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@/components/table-filter/base-use/index.vue' {
-	import { Component } from 'vue';
-	const component: Component;
-	export default component;
-}
