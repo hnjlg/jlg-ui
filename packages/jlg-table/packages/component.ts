7d86df459346bd54780f95618499a3d7db2bb8e3
DESKTOP-SGL7UB9\17680 <17680326373@163.com>
2024-05-15 00:44:36 +0800

    build: 0.0.3 yqb

--- packages/jlg-table/packages/component.ts
+++
@@ -1,4 +0,0 @@
-import type { Plugin } from 'vue';
-import { JlgGrid, JlgDynamicModal, JlgTableFilter } from './components';
-export default [JlgGrid, JlgDynamicModal, JlgTableFilter] as Plugin[];
-export {};
--- packages/jlg-table/packages/index.ts
+++ packages/jlg-table/packages/index.ts
@@ -1,6 +1,10 @@
-import components from './component';
+import { JlgGrid, JlgDynamicModal, JlgTableFilter } from './components';
 import { makeInstaller } from '../lib/install';
+import type { Plugin } from 'vue';
+
+export const components = [JlgGrid, JlgDynamicModal, JlgTableFilter] as Plugin[];
 
 const install = makeInstaller(components);
-export * from './component';
+export * from './components';
+export * from '../lib/config';
 export default install;
